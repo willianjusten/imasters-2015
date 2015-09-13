f3e990e8434d9dcaff84215bfab9eb3687165885
willianjusten <willianjustenqui@gmail.com>
2015-09-13 14:33:02 -0300

    Start lecture

---
+++ Readme.md
@@ -0,0 +1,23 @@
+# Slides SEMCOMP - 2015
+
+## Instalando
+
+[Veja os slides](http://willianjusten.com.br/semcomp-2015/).
+
+Primeiro de tudo, instale as depdências para rodar os slides em sua máquina.
+
+- [NodeJS](http://nodejs.org/)
+- [GulpJS](http://gulpjs.com/)
+
+```sh
+# Clone o repositório
+$ git clone git://github.com/willianjusten/semcomp-2015
+$ cd semcomp-2015
+
+# Instale as dependências do Node
+$ npm install
+```
+
+Com os comandos acima, seu projeto já está instalado e funcional. Basta digitar `gulp` para compilar o código e iniciar a apresentação.
+
+Esse projeto foi construído utilizando o [Reveal Boilerplate](https://github.com/willianjusten/reveal-boilerplate)
---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+Reveal.initialize({
+	controls: true,
+	progress: false,
+	history: true,
+	center: true,
+
+	transition: 'fade', // none/fade/slide/convex/concave/zoom
+
+	// Optional reveal.js plugins
+	dependencies: [
+		{ src: '/js/vendor/classList.js', condition: function() { return !document.body.classList; } },
+		{ src: 'js/vendor/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
+		{ src: 'js/vendor/notes/notes.js', async: true }
+	]
+});
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-58087941-1', 'auto');
+ga('send', 'pageview');
